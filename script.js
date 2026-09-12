a80e52b8378a9a584c18c248080179a72920c547
AvinashMudunuru <avinashmudunuru@icloud.com>
2026-09-12 11:37:32 +0530

    Add mousemove event to create a trailing effect

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("mousemove", function(e) {
+  let trail = document.createElement("div");
+  trail.className = "trail";
+  trail.style.left = e.pageX + "px";
+  trail.style.top = e.pageY + "px";
+  document.body.appendChild(trail);
+
+  setTimeout(() => {
+    trail.remove();
+  }, 500);
+});
